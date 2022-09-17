ccf627b8c3c5cdc5f00343cde011052cea768cb3
vrnmotorina <111017592+vrnmotorina@users.noreply.github.com>
2022-09-17 16:12:33 +0300

    HW6

--- Program.cs
+++ Program.cs
@@ -1,69 +1,30 @@
-﻿/*  //Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
- int size = 12;
- int minValue = 100;
- int maxValue = 999;
- int[] array = new int[size];
-
- for (int i = 0; i <array.Length; ++i)
- {
-    array[i] = new Random().Next(minValue, maxValue + 1);
- }
-
-Console.WriteLine(string.Join(",", array));
-
-int result = 0;
-for(int i = 0; i < array.Length; ++i)
-{
-    if (array[i] % 2 == 0)
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите число M");  
+ int m = Convert.ToInt32( Console.ReadLine() );
+ int sum = 0;
+for (int i = 0; i < m; i++)
     {
-result += 1;
+     Console.WriteLine("Введите число: "); 
+     int a = Convert.ToInt32( Console.ReadLine() );
+     if (a > 0)
+        { 
+        sum++;
+        }
     }
-}
-Console.WriteLine("Количество четных чисел в массиве - " + result); */
+Console.WriteLine("Количество положительных чисел = {0}", sum);
+Console.ReadKey();
 
-/* //Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-int size = 10;
- int minValue = 1;
- int maxValue = 999;
- int[] array = new int[size];
+/* // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
- for (int i = 0; i <array.Length; ++i)
- {
-    array[i] = new Random().Next(minValue, maxValue + 1);
- }
+double x = (b1 - b2)/(k2 - k1);
+double y = k2 * x + b2;
 
-Console.WriteLine(string.Join(",", array));
-
-int sum = 0;
-
-for (int i = 1; i <array.Length; i++)
-{
-    if(i%2 == 1)
-    {
-    sum += array[i];
-    }
-}
-Console.WriteLine(sum); */
-// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-/* int size = 5;
-double[] array = new double[size];
-double min = 1;
-double max = 0;
-
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().NextDouble();
-    if (array[i] > max)
-    {
-        max = array[i];
-    }
-    if (array[i]< min)
-    {
-        min = array[i];
-    }
-}
-Console.WriteLine(string.Join(",", array));
-Console.WriteLine("Максимальный элемент - " + Math.Round(max,2));
-Console.WriteLine("Минимальный элемент - " + Math.Round(min,2));
-Console.WriteLine("Разница между максимальным и минимальным элементами - " + Math.Round((max-min),2));
- */
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}"); */

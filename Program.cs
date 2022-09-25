9e993f722d701e9a4c61cc0fa49a6b53b4b3d4ce
vrnmotorina <111017592+vrnmotorina@users.noreply.github.com>
2022-09-25 17:01:38 +0300

    Update Program.cs
    hw8

--- Program.cs
+++ Program.cs
@@ -1,42 +1,5 @@
-//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
-double[,] GetArray(int m, int n) 
-{
-    double[,] result = new double[m, n];
-    for (int i = 0; i < m; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            result[i, j] = new Random().NextDouble();
-        }
-    }
-    return result;
-}
-
-void PrintArray(double[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); ++i)
-    {
-        for (int j = 0; j < array.GetLength(1); ++j)
-        {
-           Console.Write(Math.Round(array[i, j], 1)+"; ");
-        }
-        Console.WriteLine();
-    }
-}
-
-Console.WriteLine("Введите количество строк");
-int rows = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Введите количество столбцов");
-int columns = int.Parse(Console.ReadLine());
-
-double[,] result = GetArray(rows, columns);
-PrintArray(result);
-
-
-// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-/*   int[,] GetArray(int m, int n, int minValue, int maxValue) 
+//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     int[,] result = new int[m, n];
     for (int i = 0; i < m; ++i)
@@ -67,67 +30,243 @@ int rows = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов");
 int columns = int.Parse(Console.ReadLine());
 
-int[,] result = GetArray(rows, columns, 0, 9);
-PrintArray(result);
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
 
-Console.WriteLine("Введите номер строки - ");
-int m = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Введите номер столбца -");
-int n = int.Parse(Console.ReadLine());
-
-if(m < result.GetLength(0) && n < result.GetLength(1))
+for (int i = 0; i < array.GetLength(0); i++)
 {
-Console.WriteLine(result[m, n]);
+   
+    for(int j = 1; j <array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
 }
-else
+ PrintArray(array);
+ 
+
+// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+/* int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-Console.WriteLine("Такой элемент отсутствует");  
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+           Console.Write(array[i, j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 15);
+PrintArray(array);
+Console.WriteLine();
+
+int sum = 999;
+int index = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int temp = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp += array[i, j];
+    }
+    if (temp < sum)
+    {
+        sum = temp;
+        index = i;
+    }
+}
+Console.WriteLine("Строка с наименьшей суммой: " +index);
+ */
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+/* int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+           Console.Write(array[i, j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 15);
+PrintArray(array);
+Console.WriteLine();
+
+int[,] array2 = GetArray(rows, columns, 0, 15);
+PrintArray(array2);
+Console.WriteLine();
+
+int[,] arrayNew = new int[rows, columns];
+
+for (int i = 0; i < array.GetLength(0); ++i)
+{
+    for (int j = 0; j < array.GetLength(1); ++j)
+    {
+        arrayNew[i, j] = array[i, j]*array2[i,j];
+    }
+}
+PrintArray(arrayNew);  */
+
+//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+/* Console.WriteLine($"Введите размер массива X x Y x Z:");
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+Console.WriteLine($"");
+
+int[,,] array = new int[x, y, z];
+CreateArray(array);
+PrintArray(array);
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void PrintArray (int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+          Console.Write( $"({i},{j},{k}){array[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void CreateArray(int[,,] array)
+{
+  int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array.GetLength(0); x++)
+  {
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+      for (int z = 0; z < array.GetLength(2); z++)
+      {
+        array[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
 } */
 
-// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-/* int[,] GetArray(int m, int n, int minValue, int maxValue) 
+// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+/* int n = 4;
+int[,] array = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= array.GetLength(0) * array.GetLength(1))
 {
-    int[,] result = new int[m, n];
-    for (int i = 0; i < m; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
-        }
-    }
-    return result;
+  array[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= array.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > array.GetLength(1) - 1)
+    j--;
+  else
+    i--;
 }
 
-void PrintArray(int[,] array)
+WriteArray(array);
+
+void WriteArray (int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); ++i)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); ++j)
-        {
-           Console.Write(array[i, j]+" ");
-        }
-        Console.WriteLine();
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
     }
+    Console.WriteLine();
+  }
 }
-
-Console.WriteLine("Введите количество строк");
-int rows = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Введите количество столбцов");
-int columns = int.Parse(Console.ReadLine());
-
-int[,] result = GetArray(rows, columns, 0, 9);
-PrintArray(result);
-
-
-for(int j= 0; j < result.GetLength(1); j++)
-{
-    int a=0;
-    for(int i= 0; i < result.GetLength(0); i++)
-    {
-     a += result[i, j];
-    }
-     Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {(float)a/rows}");   
-}
-  */
+ */
